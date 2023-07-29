32896cc0f2ceda8d8a2ee4ac1b5d0c700fb3ec4f
zerocool <one1970zero@mail.ru>
2023-07-29 21:58:01 +0300

    Clean Architecture Presentation-слой. Отвечает за отображение и взаимодействие с пользователем. Методы бизнес-логики вызываются из UseCase. С domain-слоем взаимодействует ViewModel. Presentation-слой знает все о Domain слое, но ничего не знает о Data-слое

---
+++ app/src/main/java/ru/zerocool/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package ru.zerocool.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.zerocool.data.ShopListRepositoryImpl
+import ru.zerocool.domain.DeleteShopItemUseCase
+import ru.zerocool.domain.EditShopItemUseCase
+import ru.zerocool.domain.GetShopListUseCase
+import ru.zerocool.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){
+        deleteShopItemUseCase.shopElemRemove(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
