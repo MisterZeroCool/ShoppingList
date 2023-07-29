3ab5152f4c376a2ffea71bf122877c084a362db2
zerocool <one1970zero@mail.ru>
2023-07-29 20:42:21 +0300

    Clean Architecture Data слой. Зависит от Domain слоя и представляет реализацию репозитория ShopListRepository

--- app/src/main/java/ru/zerocool/domain/ShopListRepository.kt
+++ app/src/main/java/ru/zerocool/domain/ShopListRepository.kt
@@ -1,9 +1,9 @@
 package ru.zerocool.domain
 
 interface ShopListRepository {
-    fun addShopItem(id: ShopItem)
+    fun addShopItem(shopItem: ShopItem)
     fun shopElemRemove(shopItem: ShopItem)
     fun editShopItem(shopItem: ShopItem)
-    fun getShopElemById(shopItem: Int): ShopItem
+    fun getShopElemById(shopItemId: Int): ShopItem
     fun getShopList(): List<ShopItem>
 }
